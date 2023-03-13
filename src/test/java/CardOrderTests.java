63afc5c9ad8004c7672ed6a96bfd7ab61e434ba7
Victor Balandin <eparis@atlassian.com>
2023-03-13 13:41:28 +0300

    Success positive test

--- src/test/java/CardOrderTests.java
+++ src/test/java/CardOrderTests.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardOrderTests {
     private WebDriver driver;
 
@@ -34,7 +35,17 @@ public class CardOrderTests {
     }
 
     @Test
-    void shouldTest (){
+    void shouldPositiveTest() {
+        driver.get("http://localhost:9999");
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input ")).sendKeys("Виктор Баландин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79153366655");
+        driver.findElement(By.cssSelector("[data-test-id= agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
 
     }
 }
